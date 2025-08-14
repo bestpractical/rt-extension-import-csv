bad83d055dedea8ca344d403ee6a5df1f196f3e6
Brad Embree <brad@bestpractical.com>
2025-08-14 15:17:05 -0700

    Prepare version 0.04

--- lib/RT/Extension/Import/CSV.pm
+++ lib/RT/Extension/Import/CSV.pm
@@ -5,7 +5,7 @@ package RT::Extension::Import::CSV;
 use Text::CSV_XS;
 use Test::MockTime 'restore_time';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 our( $CurrentRow, $CurrentLine, $UniqueFields );
 
